36f69d565a6198308e00af69a7c8d5ddc1d5ae50
Maxime Valencik <valencikmaxime@gmail.com>
2021-09-15 22:59:54 -0400

    final update to algorithm and add of board file

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ using std::vector;
 using std::abs;
 
 //state of the cells in the grid 
-enum class State {kEmpty, kObstacle, kClosed};
+enum class State {kEmpty, kObstacle, kClosed, kStart, kFinish};
 
 
 vector<State> ParseLine(string line) {
@@ -64,7 +64,43 @@ int Heuristic (int x1, int y1, int x2, int y2){
     return manhattanDist;
 }
 
-// Write the AddToOpen function here.
+// CheckValidCell check that the 
+// cell is on the grid and not an obstacle (i.e. equals kEmpty).
+bool CheckValidCell (int x, int y, vector<vector<State>> &grid){
+  if (y<grid[0].size() && x<grid.size()){
+    return grid[x][y]==State::kEmpty; // true if cell empty and on grid
+  } else{
+    return false;
+  }
+}
+
+
+// directional deltas
+const int delta[4][2]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+//Expand current nodes's neighbors and add them to the open list.
+void ExpandNeighbors(const vector<int> &currentNode, int goal[2], vector<vector<int>> &openlist, vector<vector<State>> &grid) {
+  // Get current node's data.
+  int x = currentNode[0];
+  int y = currentNode[1];
+  int g = currentNode[2];
+
+  // Loop through current node's potential neighbors.
+  for (int i = 0; i < 4; i++) {
+    int x2 = x + delta[i][0];
+    int y2 = y + delta[i][1];
+
+    // Check that the potential neighbor's x2 and y2 values are on the grid and not closed.
+    if (CheckValidCell(x2, y2, grid)) {
+      // Increment g value and add neighbor to open list.
+      int g2 = g + 1;
+      int h2 = Heuristic(x2, y2, goal[0], goal[1]);
+      AddToOpen(x2, y2, g2, h2, openlist, grid);
+    }
+  }
+}
+
+
+// AddToOpen function
 void AddToOpen(int x, int y, int g, int h, vector<vector<int>> &openNode, vector<vector<State>> &grid){
     // Add node to open vector, and mark grid cell as closed.
     openNode.push_back(vector<int>{x, y, g, h});
@@ -101,10 +137,10 @@ vector<vector<State>> Search (vector<vector<State>> grid, int origin [2], int go
       if (x==goal[0] && y==goal[1]){
         return grid;
       }
+      // Expand search to current node's neighbors
+      ExpandNeighbors(current, goal, open, grid);
     }
-    // If we're not done, expand search to current node's neighbors. This step will be completed in a later quiz.
-    // ExpandNeighbors
-  
+    
   // Ran out of new nodes to explore and haven't found a path.
     cout << "No path found!" << "\n";
     return {};
@@ -114,6 +150,9 @@ vector<vector<State>> Search (vector<vector<State>> grid, int origin [2], int go
 string CellString(State cell) {
   switch(cell) {
     case State::kObstacle: return "⛰️   ";
+    case State::kPath: return "🚗   ";
+    case State::kStart: return "🚦   ";
+    case State::kFinish: return "🏁   ";
     default: return "0   "; 
   }
 }
